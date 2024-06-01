95a696453e1a08e2fb96ce09322e88343d8dbdb0
Paul Ndambo <164306210+ndambopaul@users.noreply.github.com>
2024-06-01 12:21:12 +0300

    fixes

---
+++ apis/orders/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from apps.pos.models import Order, OrderItem
+
+class PlaceOrderSerializer(serializers.Serializer):
+    items = serializers.JSONField(default=list)
+    total_cost = serializers.DecimalField(max_digits=100, decimal_places=2)
+
+    def save(self, **kwargs):
+        items = self.validated_data.get("items")
+        total_cost = self.validated_data.get("total_cost")
+        user = self.context.get("user")
+        print(user.email)
+        order_items = []
+
+        for item in items:
+            print(item)
+        return {}
--- apis/urls.py
+++ apis/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 from apis.products.views import ProductListAPIView, ProductImageAPIView
+from apis.orders.views import PlaceOrderAPIView
+from rest_framework_simplejwt.views import TokenVerifyView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 urlpatterns = [
+    path('users/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path("products/", ProductListAPIView.as_view(), name="products"),
-    path("products/<int:product_id>/images/", ProductImageAPIView.as_view(), name="images"),
+    path("products/images/", ProductImageAPIView.as_view(), name="images"),
+    path("place-order/", PlaceOrderAPIView.as_view(), name="place-order"),
 ]
